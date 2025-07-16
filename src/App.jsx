8edf062f59475cde5c1f4cd1eedf547e66f243e4
jose <jernestocastro01@gmail.com>
2025-07-16 19:10:33 -0400

    Addying declarative ReactRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Layout from "./components/template/Layout";
 import PokemonList from "./components/organisms/PokemonList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <Layout>
-      <PokemonList />
+      <Routes>
+        <Route path="/" element={<PokemonList />} />
+        {/* <Route path="/" element={<whoIsPokemon />} />
+        <Route path="/" element={<PokemonInfo />} /> */}
+      </Routes>
     </Layout>
+    </BrowserRouter>
   );
 }
 
